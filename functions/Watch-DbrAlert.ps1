9df412df07976599ab561c955ac1126ceacbc703
ctrlbold <clemaire@gmail.com>
2016-08-31 22:56:52 -1000

    got the connection attempts working :)

--- functions/Watch-DbrAlert.ps1
+++ functions/Watch-DbrAlert.ps1
@@ -83,7 +83,52 @@ Note that the PowerShell window will disappear.
 			return $bitmap
 		}
 		
-		function New-RunSpace
+		function New-ConnectivityRunSpace
+		{
+			Param (
+				[string]$sqlserver
+			)
+			
+			$scriptblock = {
+				Param (
+					[string]$sqlserver
+				)
+				
+				try
+				{
+					$testConnect = New-Object Microsoft.SqlServer.Management.Smo.Server $sqlserver
+					$testConnect.ConnectionContext.ConnectTimeout = 2
+					$testConnect.ConnectionContext.Connect()
+					$testConnect.ConnectionContext.Disconnect()
+					return "Healthy"
+				}
+				catch
+				{
+					$exception = ($_.Exception.InnerException.InnerException).ToString()
+					
+					if ($exception -like "*A network-related or instance-specific error*")
+					{
+						return "Alarms"
+					}
+					elseif ($exception -like "*Login failed*")
+					{
+						return "Healthy"
+					}
+					else
+					{
+						return "Warnings"
+					}
+				}
+			}
+			
+			$runspace = [PowerShell]::Create()
+			$null = $runspace.AddScript($scriptblock)
+			$null = $runspace.AddArgument($sqlserver)
+			$runspace.RunspacePool = $script:pool
+			return $runspace
+		}
+		
+		function New-MonitorRunSpace
 		{
 			Param (
 				[string]$sql,
@@ -125,7 +170,7 @@ Note that the PowerShell window will disappear.
 			$script:pool = [RunspaceFactory]::CreateRunspacePool(1, [int]($env:NUMBER_OF_PROCESSORS) + 1)
 			$pool.ApartmentState = "MTA"
 			$pool.Open()
-			$runspaces = @()
+			$connectionrunspaces = $runspaces = @()
 			$alertjson = Get-AlertConfig
 			$health = $errors = $warnings = 0
 			$monitors = 'Online', 'DiskSpace', 'JobStatus', 'SuspectPage', 'FullBackup', 'LogBackup'
@@ -138,79 +183,46 @@ Note that the PowerShell window will disappear.
 				{
 					'Online'
 					{
-						$servers = Get-Instances
-						
-						foreach ($testserver in $servers)
+						foreach ($servername in (Get-Instances))
 						{
-							try
-							{
-								$testConnect = New-Object Microsoft.SqlServer.Management.Smo.Server $testserver.Name
-								$testConnect.ConnectionContext.ConnectTimeout = 2
-								$testConnect.ConnectionContext.Connect()
-								$testConnect.ConnectionContext.Disconnect()
-								$health++
-							}
-							catch
-							{
-								$exception = ($_.Exception.InnerException.InnerException).ToString()
-								
-								if ($exception -like "*A network-related or instance-specific error*")
-								{
-									write-warning "error"
-									$errors++
-								}
-								elseif ($exception -like "*Login failed*")
-								{
-									write-warning "health"
-									$health++
-								}
-								else
-								{
-									write-warning "warn"
-									$warnings++	
-								}
-							}
-						}
-						
-						$tempitemsource += [PSCustomObject]@{
-							'Monitor' = $monitor
-							'Healthy' = $health
-							'Warnings' = $warnings
-							'Alarms' = $errors
+							$runspace = New-ConnectivityRunSpace -sqlserver $servername.Name
+							$connectionrunspaces += [PSCustomObject]@{ Pipe = $runspace; Status = $runspace.BeginInvoke() }
 						}
 					}
 					
 					'DiskSpace'
 					{
-						$runspace = New-RunSpace -Sql $sql -Monitor $monitor
+						$runspace = New-MonitorRunSpace -Sql $sql -Monitor $monitor
 						$runspaces += [PSCustomObject]@{ Pipe = $runspace; Status = $runspace.BeginInvoke() }
 					}
 					
 					'JobStatus'
 					{
-						$runspace = New-RunSpace -Sql $sql -Monitor $monitor
+						$runspace = New-MonitorRunSpace -Sql $sql -Monitor $monitor
 						$runspaces += [PSCustomObject]@{ Pipe = $runspace; Status = $runspace.BeginInvoke() }
 					}
 					
 					'SuspectPage'
 					{
-						$runspace = New-RunSpace -Sql $sql -Monitor $monitor
+						$runspace = New-MonitorRunSpace -Sql $sql -Monitor $monitor
 						$runspaces += [PSCustomObject]@{ Pipe = $runspace; Status = $runspace.BeginInvoke() }
 					}
 					
 					'FullBackup'
 					{
-						$runspace = New-RunSpace -Sql $sql -Monitor $monitor
+						$runspace = New-MonitorRunSpace -Sql $sql -Monitor $monitor
 						$runspaces += [PSCustomObject]@{ Pipe = $runspace; Status = $runspace.BeginInvoke() }
 					}
 					
 					'LogBackup'
 					{
-						$runspace = New-RunSpace -Sql $sql -Monitor $monitor
+						$runspace = New-MonitorRunSpace -Sql $sql -Monitor $monitor
 						$runspaces += [PSCustomObject]@{ Pipe = $runspace; Status = $runspace.BeginInvoke() }
 					}
 				}
 			}
+			
+			# Finish up T-SQL query-based runspaces
 			while ($runspaces.Status.IsCompleted -notcontains $true) { }
 			
 			foreach ($runspace in $runspaces)
@@ -218,6 +230,28 @@ Note that the PowerShell window will disappear.
 				# EndInvoke method retrieves the results of the asynchronous call
 				$tempitemsource += $runspace.Pipe.EndInvoke($runspace.Status)
 				$runspace.Pipe.Dispose()
+			}
+			
+			# Finish up connection attempt runspaces
+			while ($connectionrunspaces.Status.IsCompleted -notcontains $true) { $health = $warnings = $alarms = 0 }
+			
+			foreach ($runspace in $connectionrunspaces)
+			{
+				switch ($runspace.Pipe.EndInvoke($runspace.Status))
+				{
+					"Healthy" { $health++ }
+					"Warnings" { $warnings++ }
+					"Alarms" { $alarms++ }
+				}
+				
+				$runspace.Pipe.Dispose()
+			}
+			
+			$tempitemsource += [PSCustomObject]@{
+				'Monitor' = "Online"
+				'Healthy' = $health
+				'Warnings' = $warnings
+				'Alarms' = $alarms
 			}
 			
 			$pool.Close()
@@ -277,8 +311,9 @@ Note that the PowerShell window will disappear.
 	
 	PROCESS
 	{
-		# currently don't have yellow icon
+		Write-Output "Please wait one moment while we perform the initial population of data :)"
 		
+		# currently don't have yellow icon
 		$base64logo = "iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAABGdBTUEAALGPC/xhBQAAAAlwSFlzAAAOwgAADsIBFShKgAAAABh0RVh0U29mdHdhcmUAcGFpbnQubmV0IDQuMC41ZYUyZQAABa9JREFUWEftV1tMVFcUVdMYKvGnJn4gGk36ZfpBouGhFgeRcaC+sLZ+NKlJST9MitW2gigVRXwr1aqhrU36SKzVaJtYY9GgVatFEBAZKBQQGebhvN937jCv1b3PPARTEbUmfriTlXPvmXvuWmftffbMjHkZL0TMmTMnZdKkSYrnialTp75ZUVHxSoxyeCQlJf02duxYjBs37rmBaDB58uTCKONDkZycXDt9+nTMmzcPc+fOFcjOzobBYIDRaPxfkJKSAnLh3Rjl8IgLUCqVUKlUQghDo9FAp9PBYrGIke97e3vFtV6vFyPDbrcPg8lkgtlsHoYpU6aMLIAfyMjIQHp6OmbOnIm0tDSx0GaziZc6nU4Bh8Mh5niUJCkBj8cj4Ha7hVAWEV/L42MFzJgxDTk5OcjNzRX2swO8kF/ocrkQDocFZFmG1WoV4GueC4VC4lmeY5F+v1/MDw4OipHvHytg2rSogKysLMyePVs4wTviCAQCYpeJl0oe/HqoGpdtQZhdHvE5C91x1497UgiBcASRSESsYVGjcoA+RF5eHhQKBTIzM0lIRkIA26jVagVRVIAXO09dwBV7CEG6DgaD8Pl8uGlw4KrWjmtaG/odUWHsHrsyqhpgYuoJwoXMjHSRRy5ArgW2keH1esU9f8bXPMep4LmHn2XwJngDoxIwa9asBDgFrNznk+D0RGuALfURWZ1lEBfMg9Do7iDQfxk2kyxE8I6t/qCoCXaF77lYR+3AwwJYvVz3FUqPHYEUDNMOrfD5B5F80YXxF5zYf3YdOk5loaWuU6RAr+3E7d5+BChNTsktBHBw2p5KABeWX9MDx8mvEaZdSW4bZCLSVX4EbelqfHe+GJVtZbjV1CDsZkJ2gsXEC5aL8akFcD4ZXMVsIxPwy7mz8RwXGM+x3XHLhyKeFl7zVAKYQPZHzzojXnT80vgcQ6a817V6UNviwblGB35vcqO22Y2z9dYYLHgj50NkvFdToyzv/iCKnvfnV9xLGlGAaEJUB0ZuPFRMeoM+egyJFJpzIr/o/AZn6r14+1AEK74I4p2Dfizf58HSPQ4s2WXF4iojCip1yN/aD9WWXijLu5BX1o6FpXegqtJmjiiAbY63WgE6ETze90cQaqvGgExC2r/E1tMSVhwMYUW1HxZ3BKev27FkpwWLdwwlv4tF5f8QeYcgX1jSKq88MPDqiAI413zeh1rOKXCQ5c5famDp64XUUItVhzwoJPLl+7w4/qdfGPNtbZxc84B8E5FvbEPuhhbklrbWC3KOkWrAH6RGE6JzTjDajCIFfM7l1noE9P3ounQDhQdkLN8vYdleF5butlFKJERIxNGzuhh5N5Sb/o6Sl7RgwaeNfF0do3+0AK58SfLB6/HBJ/kheX0JAewGH7HaZm+CfMkuG9luorwbcL7JQ89EsPvnvuHknzUiZ3098sq7V8boHy2AWyuLGArubAz+kuEWu/eMmcjdtHM7Fu80463tBhRsG8CqPRrYPdQRQ2HklzUQ+W0iv4WcT+qhWH8jUrhbSo3RRwWkpqYK0jj4+6CtrQ3t7e0jouiI6QF51X0i16LsByOsriC8chC7fuoROU+Qr7tOY2NfjDoaEydOPEmD+N32JBg/4TUitoiKZ/KlVTqcuOYEOY9OjRur99JxK2kl8iYivSnIs9de4fsfaf2DoF+rE4qKil5/UiwrvVQcJy86bESnlntEBCcu67Fyewc+runB2qNdKD7cgWWbG4j8KrKL/0BuWceaGPWzRcE2zf54o+k3BWB2yCg51iMazfcXjeREJIHjF/sEOTtQUKVLi73i2aKgUns93mjWHB1AYWXnkEbTjAIqvvzSG1BtuAbFWiInAYp1fznnV+C//x88aeRXaE4RuVq1pU+96PNutXJzl1q5qUO9cKNaTVWvXrChWU3Fp6b8q+noCdBx3B5b/jJehBgz5l+wOUNwWzGpVgAAAABJRU5ErkJggg=="
 		$base64red = "iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAFRUlEQVR4Xu3aeWwUVRwH8O+bY4+ekoj+ZYwhMQGUQCptWhttsy1YIx4YTACNRyMpisVgsR5UEWksqFSJogTiQf0DTLQFJcbShQpJjy2IQUVMJMT/aE0bkm6P7c7OmFlT4nTr7sy7iGn7Z/f93u99PzM783ZnCWb4H5nh+TELMHsGXCOBaKhoEYFVbMJSVZCzwXCkhwCW7OVIfwsMVy2Za8X1zxQL9zrDkkgioazN7+z+QyaCVIBoqOhGWIkTIMr86UOa/RbRynM7en6ThSANIHP4ychyEaQAuA8vH0E4gPfwchGEAtCHl4cgDIA9vBwEIQDJ8DCOA+oCPldzcRdG7gD8w4s9E7gCiAsvDoEbgPjwYhC4AMgLzx+BGUB+eL4ITADDy0puIInYCa9Xe1/NRpDr5zpuENbgICY+avZ402C/O1AD0Ia3Ewb3tkCZd6sjrHnpIsaeXuMRwB7OhkAFwBI+CbB7P5QFtzsBfj+PsWefpABgQ/AMwBreXm7gnT1QFxc4wibOncX4phpKAHoETwA8wicBGpuhFpU4AU73YvylWgYAOgTXAP+Et7e3WMi4Svi3NkErLXcCdJ3C+Gt1rFN7via4AuAZ3k7of/kNaKF7HGGNzmOIbd/CAcDbmZARgHf4JMALr0Krut8J0H4UsZ3bOAG4R0gLICK8vTTfhjroD65yAnzbith7TRwB3CH8J4Co8EmAdbXQH1nrCBv/6iDFRsiNV/p9wrQAV0pL52i+iZMguM1NC69jfE+sg/5otRPg4OeY2L/H61Qux5v9hmXded3xMxenFkwLEC0v/AIKnIfIZSs3w/TVj8NX/Yxj6MSBfYgf2O+mnGqMBasnJ9xXMvXhSwpA8sHFuH5ZUaBQdXJRpD+8Gr71zzsB9n2A+KEWF9X0QwiswuxwX9+/Z0gBiFYuDcEkHfRtMldqK1bCv7HeCfDhLsRbD2UuZhhBgJrscGRvWoDhyqV3E5N0MvTJWKotvw/+zQ2OcbHmt2AcbctYyzKAEFKd3dH7SVqAwaqiPH0i0a9ACbA0S1erlVXCv2W7E2DHVhjHvhPVMjmvqSbm57WfuZAWwH4xGircCWCzqNWoJXchsO1tJ8Cbr8D4ISyqpf3YuTU3HFk5tcG0dwFr1UJfdCj7CAGWi1iRekcRAk27HVOPN9Qh0X1KRDv7ofsvJkFZXjgy6ArAHmQVFwej2fE2YinLeK9KXbQEgV0fOwFefA6JHyO8WyXDW5oWym3vGphu8rRbYWEImgaSneNYjzUSBQyDL0CG8HazjB+GhCHwjZo6m4vwrgCEvh10H4iuwRod5cvhMrxrAN4IJBiEr7YeWnkFoOkwz/+M2LuNMP+8xA7hIbwngEmEkSzjMEAqWVbqr38dWqXzJ0LWXwMYfewhtuuAx/CeAXghZLV1gOTkphiObXgK5oVf6WwpwlMB8EAIfvollJtuTgk6uuYBWAOXvQNQhqcGYEWwvxD1NzQCqno1rPHN14i9v0NqeCYAVgTllnnQKqqArCwkTvci0XUSsDz+TpLhyE9KZ9wHZDok9j6Bx4UxU5+U1zmEZz4DJheVRAiYR6BYFZ6D0BRwCs8N4OrbQQYCx/BcAaQgcA7PHUAogoDwQgCEIAgKLwyAK4LA8EIBkggrCrJGotph6ruD4PDCAZgQJISXAkCFICm8NABPCBLDSwVwhSA5vHSAJEJZWWBEHWkyTbJeUeCz/2fCNBRCWuJGfNOczp+u0OyOaWuYPwzRNh6qKMjXibIYlkpMlZzL/757iHYulrprBsCyaJ61swA8Nf+Pc834M+Bv3q/SX94+OnUAAAAASUVORK5CYII="
 		$base64green = "iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAHU0lEQVR4XuWba4wUVRbH/+fWzAgKiuLb4CMxIhgGmPGxGKNhs0qYmaaretAPgooa95OrZBXiK+vsGhdXcV31kyYaBB8JON3V090YSHzg+4EwKCY7Zr/gC3ZxQR3Ukeq+x9weepxuqrurqm9Xd3S+debcc87/V/fcV90i/Mb/6DeuH6EAWLhx4dEtIxPmSuLpAjiHgVOkxGSDaJJ6ADnmA0JgmIDdzDQkjdwQtzqDL3W99F29H1B9ADCoa2BRp2DqBcvfS1CngDD8iJGQOSHFVgi8QlK8mIrFt4PAfnx4sdUKwEyYU7JMNzF4GRFmeknAqw0zfSKEXJNtO/ikzp6hBYAVt6ZmgT8z+GYQjvYqKogdQX7LEI87Tusjm67asC+Ij/FtagLQ19cntrbvuBGUfQAwjqs1GT/tpcTXwuCV5w/Ofaavr0/6aasFQGQgclY2J54zQPOCBtfSjvEGteaWpiKpz4L4C9QDuuNWTFDuaYY4JkhQ7W0k9sPg69JmMuXXtz8ADOpORu8kpvv9BgrFnmllOpZ4yE8szwDy9T578J8AbvUTIGxbAh7u3DFnpddxwRsABvUko/8C0y1hCwoSj5lWZ2KJFV7aegLQbUfvatpuX0YlEa9ImcnV1SBUBaAGPCLur+aoGf/PUvRkeuOZSrlVBKCmOuRoe9OM9j4p5yD/30Y0N2kmPy/XtCwANei9N3v7mw2f532KLjWXkFs2mgPzy+0jygLoiVs3gfjJGuM3RXMCrk1Z9jq3ZFwBqLW9Q9lPw17e1o+W/F8LxHTbsr8pjeEKIBK37mfiu+qXUO2e2wwDQhBGnKwnZwz8JWPZ91UFMLqlxa567+o8ZV3GSIm/+uJZaBMG1r49iBEn58Fdbt9EZ8IZG67acGC88WE9oCdurQDxgx48NsSkIP7MqVPy8b/aP+wDApanLfvR8gDUWj9h7tR9mKGLVKn4gt8v9w9jnZeekMNgerE9tyyA7oTZScBWXQnr9FNOfCHGf/67D8++81HVkJK4faOZ/LhgWFQCPXZ0FZjuqOolZINq4n90HKx9awd2f1NU3q5ZMvHfMmbyXncAich7gHFhyPoqhlPil1zcjjOmuh89+BGfD8R4Ix2zLz0MgDq6pp9a9wsI0SwAtIvPC5POwSNHjt28YPP36tdYCUTi1mVM/NqvW/yoOpJiXqo3/m4RgO6E+UcCnmgGAPV58r8oY6ZlmVjimeIekDBXM3BbowG0tRhYMk9jzbsLWpW27PxKd6wEehLmcwCubiSAkMSDJdZkeu3riwB09ZsDQiASFIAggtV5LnZ+sRdDe7727SYs8aPjIPrTvfbi0kHwFSae7ztzAEr84gtmYuapJyAnGevf/8QXhFDF5/Xz5o1WcoEWAEp87/kzcN5pJ46x8wMhbPGjHcAFQJASIPXkS8QXKHiBoMQvndeO03Utcrx2X7cSCDIIEgFWx7lon3aya+hKEBomXi0G3QbBSMBpMAiERoo/9KRcpsEazgD9QGgC8XBdCHUlF10qpNjitYxK7RQEs2MGZk87qWw52Nv+jQvOOjX8mi/JyHUpfGgztM/vVZbxvqtBqATX964u6JOCdKiFp6QiqR+KpkH1o6fffBcCFwX2rZaWVXqCm+/wxAPE9HoqlriskEfxgUjC/DuAO2sBkKeanx1mon3aL2uDcj7DFH8oh7+mLbvPFUCkP9bBQn5YKwCvEBogXg2AszKxxE5XAMgfilofE/F59YbQCPEAtqctu6No3CoVGrGjtzOTr1sWlWCp1aLVMaOoHBokHmC6NR1LPFYRwB/WX3nMxNafdul8IzweQqPEqzfFk5wjzqz6YiQ/GyRM9QrpHh1lMFZrROhqPxvbdu32dHqrM3Z+TGK6JxVLHHa3yfXd4IL1Vx5nGM6QEDhedyIN8Ue8J9d2cLrbDdOyr8e749YNRPxUQxLWHZR4SdpMPu/mtuIFiQ9mb9tCEJfozidMf8x4OWPZl/u+IKGSjKQip/NBYxACx4aZtL5Ycq8QmDMQHfiqnM+ql6R67GgETAP6kgrJkwSzwQszZnJTxWnaSzrN/srcVYPLnO9rDDhsgRTwwMQLYO02xA+kzaSnPU3VEigkp26NfdC+4x9EfLv2hPU6XJU27bu9fl3iGUAhR91LZW3apdrJ0PLSpW41/74BKIfd/bFuEs7a5rlFJvcCYmnasjdXE1z6/0AAlJOoHZ3mMK8TEGOHC36D67BX87xhyGsrTXU1zwJlHYzeIr8GzA8Bovrphw7FBR/EewDclo4mX/Ba7zXNApVyV1frHOBPhNzyepeF2tW1sPFIdsLI4zq+HgtcAm5AFiUXTZZS3IAclsHAHJ0PXB1mgGnNxGzL06Vb2lriaAUwPpEuOzqLgMUkaT5I/g4Qrf4SlQ6x8Q4Tv8pML44/xvLnp7J13QCMD3vFpiuOOuLApFnq01kins4SpxAwWQqerOyEpGEGhkmMfjormIbQ5nxUOLrWKVjbLFDPpML0HUoPCFOQ31g/AwM3Zm5vquU6AAAAAElFTkSuQmCC"
@@ -419,23 +454,22 @@ Note that the PowerShell window will disappear.
 				Update-ListView
 			})
 		
-		$buildlistviewtimer.start()
-		
 		$timer = New-Object System.Windows.Forms.Timer
 		$timer.Interval = 200000
 		$timer.add_Tick({
 				Update-WindowStatus
 			})
 		
-		$timer.start()
-		
-		<# Make PowerShell Disappear
+		# Make PowerShell Disappear
 		$windowcode = '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'
 		$asyncwindow = Add-Type -MemberDefinition $windowcode -name Win32ShowWindowAsync -namespace Win32Functions -PassThru
 		$null = $asyncwindow::ShowWindowAsync((Get-Process -Id $pid).MainWindowHandle, 0)
-		#>
+		
 		# Force garbage collection just to start slightly lower RAM usage.
 		[System.GC]::Collect()
+		
+		$buildlistviewtimer.start()
+		$timer.start()
 		
 		# Create an application context for it to all run within.
 		$appContext = New-Object System.Windows.Forms.ApplicationContext
